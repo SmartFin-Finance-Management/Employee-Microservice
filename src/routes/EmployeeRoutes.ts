6596f3a0c72face71f03f04b95979a3bc822db22
Visweshvar-UST <287618@ust.com>
2024-10-28 09:33:05 +0530

    doc part of routes

--- src/routes/EmployeeRoutes.ts
+++ src/routes/EmployeeRoutes.ts
@@ -14,13 +14,18 @@ import {
 
 const router = express.Router();
 
+// CRUD
 router.get('/employees', getAllEmployees);
 router.get('/employees/:employee_id', getEmployeeById);
 router.post('/employees', createEmployee);
 router.put('/employees/:employee_id', updateEmployee);
 router.delete('/employees/:employee_id', deleteEmployee);
+
+// Attendance
 router.post('/employees/:employee_id/attendance', addAttendance);
 router.put('/employees/:employee_id/attendance', updateAttendance);
+
+//  Filter by OrgId, ClientId, ProjectId
 router.get('/employees/organization/:org_id', getEmployeesByOrgId);
 router.get('/employees/client/:client_id', getEmployeesByClientId);
 router.get('/employees/project/:project_id', getEmployeesByProjectId);
